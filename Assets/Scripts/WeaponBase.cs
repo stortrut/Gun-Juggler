340e68ef595ee577354f89720e9cfadefc5e5300
toveandersson <toveama@gmail.com>
2023-11-20 22:30:34 +0100

    Tove Scene, test script WeaponBase

---
+++ Assets/Scripts/WeaponBase.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBase : MonoBehaviour
+{
+    public float Damage { get; set; }
+    public float Speed { get; set; }
+    public float FireRate { get; set; }
+
+    public GameObject bulletPrefab;
+
+    public float timer;
+
+    public WeaponBase(float damage, float speed, float fireRate) //constructor, method that is called when creating the object and initializes it
+    {
+        this.Damage = damage;
+        this.Speed = speed;
+        this.FireRate = fireRate;
+    }
+
+    public void Fire()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+        //add velocity
+    }
+}
+public class Weapon : WeaponBase
+{
+    public Weapon(float damage, float speed, float fireRate)
+        : base(damage, speed, fireRate)
+    {
+    }
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            timer = FireRate;
+            WeaponBase weaponBaseInstance = this;
+
+            weaponBaseInstance.Fire();
+        }
+    }
+}
+public class RunThis : MonoBehaviour
+{
+    private void Start()
+    {
+        Weapon revolver = new Weapon(10, 10, 1);
+    }
+}
