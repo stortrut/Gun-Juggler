6c5bd2a5666c2a61023f3df7263277d992549da2
toveandersson <toveama@gmail.com>
2023-11-22 09:25:07 +0100

    Weaponscript

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,12 +7,11 @@ public class Weapon : WeaponBase
 {
     [SerializeField]
     WeaponBase weaponBase;
+    private void Start()
+    {
+    }
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            weaponBase.Use(10f,2f,1f,transform.position);
-        }
     }
     public void Shoot(float damage, float speed, float fireRate, Vector3 pos)
     {
