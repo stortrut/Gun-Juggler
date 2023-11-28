51a8f303203ef4dd32e4385e39ee68e55d1427a2
Waterhuman33 <vide@eneljung.se>
2023-11-28 14:43:12 +0100

    Now less spagetti

--- Assets/Scripts/Enemy/EnemyProtection.cs
+++ Assets/Scripts/Enemy/EnemyProtection.cs
@@ -17,7 +17,7 @@ public class EnemyProtection : MonoBehaviour
         for (int i = 0; i < numberOfProtection; i++) 
         { 
           
-          currentProtection = Instantiate(Protection,new Vector2(i,i),Quaternion.identity,gameObject.transform); 
+          currentProtection = Instantiate(Protection,transform.position+new Vector3(0,0,i),Quaternion.identity,gameObject.transform); 
           protectingItems.Add(currentProtection);
         }
 
