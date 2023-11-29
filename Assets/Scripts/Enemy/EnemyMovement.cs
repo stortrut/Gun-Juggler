7799b6a44388707b9506e8b7ca8ff9e01f17f7c7
Waterhuman33 <vide@eneljung.se>
2023-11-29 09:46:10 +0100

    Update EnemyMovement.cs

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour //, IStunnable
