569038bcd49c08d5b36c8edb3e5d5b6839a5a459
stortrut <feiffalvin@gmail.com>
2023-12-04 15:49:55 +0100

    player prefab update

---
+++ Assets/aoiwdhoqiwdh.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aoiwdhoqiwdh : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+[System.Serializable]
+class DialogueBase
+{
+    [SerializeField] private string input;
+    [SerializeField] private Color textColor;
+    [SerializeField] private float delay;
+}
