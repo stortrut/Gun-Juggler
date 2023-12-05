c0feacc9d1a489a3a3d1aa08e9c254f20fdd447c
Waterhuman33 <vide@eneljung.se>
2023-12-05 15:03:28 +0100

    bullet collide with ground

--- Assets/Scripts/Weapons/Bullet/Bullet.cs
+++ Assets/Scripts/Weapons/Bullet/Bullet.cs
@@ -33,8 +33,12 @@ public class Bullet : MonoBehaviour
         spriteRenderer.color = newColor;
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter2D(Collider2D other)
     {   
-        Destroy(gameObject);
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+        
     }
 }
