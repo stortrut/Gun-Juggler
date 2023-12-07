f2e873710336b3d1d14e9f41d32e13c216302cec
Waterhuman33 <vide@eneljung.se>
2023-12-07 16:14:28 +0100

    many many

--- Assets/Scripts/Manager/Checkpoint.cs
+++ Assets/Scripts/Manager/Checkpoint.cs
@@ -1,14 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Checkpoint : MonoBehaviour
 {
+    private float timeStart=200;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            Manager.Instance.Invoke("ProceedToNextLevel", .2f);
+            timeStart = Time.time;
+            Debug.Log("Started Scene Preloading");
+
+            // Start scene preloading.
+            Manager.Instance.LoadNextLevel();
+
+
         }
     }
+    private void Update()
+    {
+        if ((int)Time.time == (int)timeStart + 3)
+        {
+            Debug.Log("Allowed Scene Activation");
+
+            Manager.Instance.ProceedToNextLevel();
+        }
+        // Manager.Instance.Invoke("ProceedToNextLevel", .2f);
+    }
 }
+
+
