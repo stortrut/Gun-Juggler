88a74925acd6cb5051f493407c759bf09dec4c59
stortrut <feiffalvin@gmail.com>
2024-01-04 09:26:52 +0100

    Tutorial and watergun fix

--- Assets/HandHoldingTutorial.cs
+++ Assets/HandHoldingTutorial.cs
@@ -4,8 +4,38 @@ using UnityEngine;
 
 public class HandHoldingTutorial : MonoBehaviour
 {
+    public GameObject firstGun;
+
+    public GameObject aimBox;
+    public GameObject shootBox;
 
 
+    private void Start()
+    {
+        StartCoroutine(nameof(Tutorial));
+    }
+
+
+    IEnumerator Tutorial()
+    {
+        if(firstGun == null) { Debug.Log("NDAWONDAOWDNAOWPDIHAWDAWD"); }
+
+        while (firstGun != null)
+        {
+            yield return null;
+        }
+
+        aimBox.SetActive(true);
+
+        yield return new WaitForSeconds(4f);
+
+        shootBox.SetActive(false);
+
+
+
+
+        Debug.Log("END");
+    }
 
 
 
