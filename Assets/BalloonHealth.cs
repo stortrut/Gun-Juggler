8e2d2abcf1bdd2dddb47c518aa3e8cdd669e317a
Waterhuman33 <vide@eneljung.se>
2024-01-04 11:56:08 +0100

    less happiness per balloon pop

--- Assets/BalloonHealth.cs
+++ Assets/BalloonHealth.cs
@@ -40,7 +40,7 @@ public class BalloonHealth : Health
         {
             ApplyDamage(1);
             Score.Instance.bulletsHit++;
-            AudienceSatisfaction.Instance.AudienceHappiness(1);
+            AudienceSatisfaction.Instance.AudienceHappiness(0.5f);
            // Destroy(other.gameObject);
         }
         if(other.gameObject.CompareTag("HoolaHoop"))
